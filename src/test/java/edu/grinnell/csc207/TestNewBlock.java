486916aa33949315f30552315d1da38a6e02b04e
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2024-09-18 22:03:38 -0500

    Add skeleton for student tests.

---
+++ src/test/java/edu/grinnell/csc207/TestNewBlock.java
@@ -0,0 +1,23 @@
+package edu.grinnell.csc207;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests of the new block.
+ */
+public class TestNewBlock {
+  // +-------+-------------------------------------------------------
+  // | Tests |
+  // +-------+
+
+  /**
+   * A placeholder.
+   */
+  @Test
+  public void placeholder() {
+    assertEquals("hello", "hello");
+  } // placeholder()
+
+} // class TestNewBlock
