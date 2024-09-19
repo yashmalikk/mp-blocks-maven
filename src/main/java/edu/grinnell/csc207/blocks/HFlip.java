9ca2684007d9f1b0a3642fe5c0ceebb17655484c
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2024-09-18 20:46:59 -0500

    Add templates for HFlip and VFlip

---
+++ src/main/java/edu/grinnell/csc207/blocks/HFlip.java
@@ -0,0 +1,68 @@
+package edu.grinnell.csc207.blocks;
+
+/**
+ * A horizontally flipped ASCII block.
+ *
+ * @author Samuel A. Rebelsky
+ * @author Your Name Here
+ */
+public class HFlip implements AsciiBlock {
+  // +--------+------------------------------------------------------------
+  // | Fields |
+  // +--------+
+
+  /**
+   * The original block.
+   */
+  AsciiBlock block;
+
+  // +--------------+------------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  /**
+   * Build a new block with the specified contents.
+   *
+   * @param original
+   *   The original block.
+   */
+  public HFlip(AsciiBlock original) {
+    this.block = original;
+  } // HFlip(AsciiBlock)
+
+  // +---------+-----------------------------------------------------------
+  // | Methods |
+  // +---------+
+
+  /**
+   * Get one row from the block.
+   *
+   * @param i the number of the row
+   *
+   * @return row i.
+   *
+   * @exception Exception
+   *   If the row is invalid.
+   */
+  public String row(int i) throws Exception {
+    throw new Exception("Not yet implemented"); // STUB
+  } // row(int)
+
+  /**
+   * Determine how many rows are in the block.
+   *
+   * @return the number of rows
+   */
+  public int height() {
+    return 0;   // STUB
+  } // height()
+
+  /**
+   * Determine how many columns are in the block.
+   *
+   * @return the number of columns
+   */
+  public int width() {
+    return 0;   // STUB
+  } // width()
+} // class HFlip
