5edc41e4a2ea2f5f1ddd64b26fe064de37289f36
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2024-09-18 21:16:36 -0500

    Trim -> Trimmed

---
+++ src/main/java/edu/grinnell/csc207/blocks/Trimmed.java
@@ -0,0 +1,101 @@
+package edu.grinnell.csc207.blocks;
+
+/**
+ * A trimmed ASCII block.
+ *
+ * @author Samuel A. Rebelsky
+ * @author Your Name Here
+ */
+public class Trimmed implements AsciiBlock {
+  // +--------+------------------------------------------------------------
+  // | Fields |
+  // +--------+
+
+  /**
+   * The original block.
+   */
+  AsciiBlock block;
+
+  /**
+   * Which part of the block do we keep horizontally?
+   */
+  HAlignment halign;
+
+  /**
+   * Which part of the block do we keep vertically?
+   */
+  VAlignment valign;
+
+  /**
+   * How much of the block do we keep horizontally?
+   */
+  int width;
+
+  /**
+   * How much of the block do we keep vertically?
+   */
+  int height;
+
+  // +--------------+------------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  /**
+   * Build a new block with the specified contents.
+   *
+   * @param original
+   *   The original block.
+   * @param horiz
+   *   How the trimmed block is horizontally aligned on the original.
+   * @param vert
+   *   How the trimmed block is vertically aligned on the original.
+   * @param trimmedWidth
+   *   The width of the trimmed block.
+   * @param trimmedHeight
+   *   The height of the trimmed block.
+   */
+  public Trimmed(AsciiBlock original, HAlignment horiz, VAlignment vert,
+      int trimmedWidth, int trimmedHeight) {
+    this.block = original;
+    this.halign = horiz;
+    this.valign = vert;
+    this.width = trimmedWidth;
+    this.height = trimmedHeight;
+  } // Trimmed(AsciiBlock, HAlignment, VAlignment, int, int)
+
+  // +---------+-----------------------------------------------------------
+  // | Methods |
+  // +---------+
+
+  /**
+   * Get one row from the block.
+   *
+   * @param i the number of the row
+   *
+   * @return row i.
+   *
+   * @exception Exception
+   *   If the row is invalid.
+   */
+  public String row(int i) throws Exception {
+    throw new Exception("Not yet implemented"); // STUB
+  } // row(int)
+
+  /**
+   * Determine how many rows are in the block.
+   *
+   * @return the number of rows
+   */
+  public int height() {
+    return 0;   // STUB
+  } // height()
+
+  /**
+   * Determine how many columns are in the block.
+   *
+   * @return the number of columns
+   */
+  public int width() {
+    return 0;   // STUB
+  } // width()
+} // class Trimmed
