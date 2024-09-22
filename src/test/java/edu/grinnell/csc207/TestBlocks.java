461acb0cbab39775c8fc25baf9ba6620d30566ed
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2024-09-22 10:19:04 -0500

    Add tests for surrounded.

--- src/test/java/edu/grinnell/csc207/TestBlocks.java
+++ src/test/java/edu/grinnell/csc207/TestBlocks.java
@@ -5,6 +5,7 @@ import edu.grinnell.csc207.blocks.Boxed;
 import edu.grinnell.csc207.blocks.Empty;
 import edu.grinnell.csc207.blocks.Line;
 import edu.grinnell.csc207.blocks.Rect;
+import edu.grinnell.csc207.blocks.Surrounded;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -305,5 +306,199 @@ public class TestBlocks {
         TestUtils.toString(boxedRect),
         "E: Correct contents for a taller boxed rectangle");
   } // testBoxedRectChange()
+
+  /**
+   * Make sure that we can box the empty block.
+   */
+  @Test
+  public void boxedEmptyBlock() throws Exception {
+    AsciiBlock boxed = new Boxed(new Empty());
+    assertEquals(2, boxed.width(),
+        "E: Correct width for a boxed empty block");
+    assertEquals(2, boxed.height(),
+        "E: Correct height for a boxed empty block");
+    assertEquals("/\\", boxed.row(0),
+        "E: Correct row 0 of boxed empty block");
+    assertEquals("\\/", boxed.row(1),
+        "E: Correct row 1 of boxed empty block");
+  } // boxedEmptyBlock()
+
+  // +------------+--------------------------------------------------
+  // | Surrounded |
+  // +------------+
+
+  /**
+   * Testing of basic surrounded structures.
+   */
+  @Test
+  void TestSurroundedBasic() throws Exception {
+    assertNotNull(new Surrounded(new Line("Hello"), '-'),
+        "R: Can surround text lines");
+    assertNotNull(new Surrounded(new Rect('B', 3, 4), '!'),
+        "R: Can surround rectangles");
+  } // TestSurroundedBasic()
+
+  /**
+   * Testing of basic surrounded lines.
+   */
+  @Test
+  void TestSurroundedLine() {
+    AsciiBlock surroundedLine = new Surrounded(new Line("Hello"), 'x');
+    assertEquals(7, surroundedLine.width(),
+        "M: Correct width for a surrounded line");
+    assertEquals(3, surroundedLine.height(),
+        "M: Correct height for a surrounded line");
+    assertEquals("xxxxxxx\nxHellox\nxxxxxxx\n", TestUtils.toString(surroundedLine),
+        "M: Correct contents for a surrounded line");
+  } // TestSurroundedLine()
+
+  /**
+   * Testing of basic surrounded rectangles.
+   */
+  @Test
+  void TestSurroundedRect() throws Exception {
+    AsciiBlock surroundedRect = new Surrounded(new Rect('v', 3, 4), '^');
+    assertEquals(5, surroundedRect.width(),
+        "M: Correct width for a surrounded rectangle");
+    assertEquals(6, surroundedRect.height(),
+        "M: Correct height for a surrounded rectangle");
+    assertEquals("""
+                 ^^^^^
+                 ^vvv^
+                 ^vvv^
+                 ^vvv^
+                 ^vvv^
+                 ^^^^^
+                 """,
+        TestUtils.toString(surroundedRect),
+        "M: Correct contents for a surrounded rectangle");
+  } // TestSurroundedRect()
+
+  /**
+   * Test of surrounded lines that have been narrowed.
+   */
+  @Test
+  public void testSurroundedLineNarrower() {
+    Line line = new Line("Hello");
+    AsciiBlock surroundedLine = new Surrounded(line, '|');
+
+    line.update("Hi");
+    assertEquals(4, surroundedLine.width(),
+        "E: Correct width after making surrounded line narrower");
+    assertEquals(3, surroundedLine.height(),
+        "E: Correct height after making surrounded line narrower");
+    assertEquals("||||\n|Hi|\n||||\n", TestUtils.toString(surroundedLine),
+        "E: Correct contents for narrowed surrounded line");
+  } // testSurroundedLineNarrower()
+
+  /**
+   * Test of surrounded lines that have been made wider.
+   */
+  @Test
+  public void testSurroundedLineWider() {
+    Line line = new Line("X");
+    AsciiBlock surroundedLine = new Surrounded(line, 'x');
+
+    line.update("ABCDE");
+    assertEquals(7, surroundedLine.width(),
+        "E: Correct width after making surrounded line wider");
+    assertEquals(3, surroundedLine.height(),
+        "E: Correct height after making surrounded line wider");
+    assertEquals("xxxxxxx\nxABCDEx\nxxxxxxx\n", 
+        TestUtils.toString(surroundedLine),
+        "E: Correct contents for widened surrounded line");
+  } // testSurroundedLineWider()
+
+  /**
+   * Test of surrounded rects that change. We do this as one test so that
+   * we can stack changes.
+   */
+  @Test
+  public void testSurroundedRectChange() throws Exception {
+    Rect rect = new Rect('X', 3, 4);
+    AsciiBlock surroundedRect = new Surrounded(rect, 'O');
+    rect.wider();
+
+    assertEquals(6, surroundedRect.width(),
+        "E: Correct width for a wider surrounded rectangle");
+    assertEquals(6, surroundedRect.height(),
+        "E: Correct height for a wider surrounded rectangle");
+    assertEquals("""
+                 OOOOOO
+                 OXXXXO
+                 OXXXXO
+                 OXXXXO
+                 OXXXXO
+                 OOOOOO
+                 """,
+        TestUtils.toString(surroundedRect),
+        "E: Correct contents for a wider surrounded rectangle");
+
+    rect.narrower();
+    rect.narrower();
+
+    assertEquals(4, surroundedRect.width(),
+        "E: Correct width for a narrower surrounded rectangle");
+    assertEquals(6, surroundedRect.height(),
+        "E: Correct height for a narrower surrounded rectangle");
+    assertEquals("""
+                 OOOO
+                 OXXO
+                 OXXO
+                 OXXO
+                 OXXO
+                 OOOO
+                 """,
+        TestUtils.toString(surroundedRect),
+        "E: Correct contents for a narrower surrounded rectangle");
+
+    rect.shorter();
+    rect.shorter();
+
+    assertEquals(4, surroundedRect.width(),
+        "E: Correct width for a shorter surrounded rectangle");
+    assertEquals(4, surroundedRect.height(),
+        "E: Correct height for a shorter surrounded rectangle");
+    assertEquals("""
+                 OOOO
+                 OXXO
+                 OXXO
+                 OOOO
+                 """,
+        TestUtils.toString(surroundedRect),
+        "E: Correct contents for a shorter surrounded rectangle");
+
+    rect.taller();
+
+    assertEquals(4, surroundedRect.width(),
+        "E: Correct width for a taller surrounded rectangle");
+    assertEquals(5, surroundedRect.height(),
+        "E: Correct height for a taller surrounded rectangle");
+    assertEquals("""
+                 OOOO
+                 OXXO
+                 OXXO
+                 OXXO
+                 OOOO
+                 """,
+        TestUtils.toString(surroundedRect),
+        "E: Correct contents for a taller surrounded rectangle");
+  } // testSurroundedRectChange()
+
+  /**
+   * Make sure that we can surround the empty block.
+   */
+  @Test
+  public void surroundedEmptyBlock() throws Exception {
+    AsciiBlock surrounded = new Surrounded(new Empty(), 's');
+    assertEquals(2, surrounded.width(),
+        "E: Correct width for a surrounded empty block");
+    assertEquals(2, surrounded.height(),
+        "E: Correct height for a surrounded empty block");
+    assertEquals("ss", surrounded.row(0),
+        "E: Correct row 0 of surrounded empty block");
+    assertEquals("ss", surrounded.row(1),
+        "E: Correct row 1 of surrounded empty block");
+  } // surroundedEmptyBlock()
 
 } // class TestBlocks
