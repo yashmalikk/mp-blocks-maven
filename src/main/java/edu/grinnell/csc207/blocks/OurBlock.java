8c9e05cd6903eb78bc40914c82c0d35b942da244
linricha <linricha@grinnell.edu>
2024-09-26 18:48:13 -0500

    Finished Art80x24: HOWEVER, it takes too long to print, so need to rewrite code so its faster OR just wait and give finished product as a comment

--- src/main/java/edu/grinnell/csc207/blocks/OurBlock.java
+++ src/main/java/edu/grinnell/csc207/blocks/OurBlock.java
@@ -1,5 +1,6 @@
 package edu.grinnell.csc207.blocks;
 
+import java.io.PrintWriter;
 
 /**
  * An AsciiBlock where every character in it is mutated
@@ -56,6 +57,7 @@ public class OurBlock implements AsciiBlock{
    *   If the row is invalid.
    */
   public String row(int i) throws Exception {
+    java.io.PrintWriter pen = new PrintWriter(System.out, true);
     if (i < 0 || i >= height()) {
       throw new Exception("Invalid row index: " + i);
     } // if
--- src/main/java/edu/grinnell/csc207/main/Art80x24.java
+++ src/main/java/edu/grinnell/csc207/main/Art80x24.java
@@ -1,8 +1,13 @@
 package edu.grinnell.csc207.main;
 
 import edu.grinnell.csc207.blocks.AsciiBlock;
+import edu.grinnell.csc207.blocks.HAlignment;
+import edu.grinnell.csc207.blocks.HComp;
 import edu.grinnell.csc207.blocks.HFlip;
 import edu.grinnell.csc207.blocks.Rect;
+import edu.grinnell.csc207.blocks.Surrounded;
+import edu.grinnell.csc207.blocks.VAlignment;
+import edu.grinnell.csc207.blocks.VComp;
 import edu.grinnell.csc207.blocks.Lines;
 import edu.grinnell.csc207.blocks.OurBlock;
 import java.io.PrintWriter;
@@ -29,30 +34,40 @@ public class Art80x24 {
     // Create a rectangular block filled with '^'
     // AsciiBlock art = new Rect('^', 80, 24);
     String outer = new String("  mmmm  ");
-    String outerMid = new String(" m    m  ");
+    String outerMid = new String(" m    m ");
     String mid = new String("m      m");
     
     String[] miniArt = {outer, outerMid, mid, mid, outerMid, outer};
 
     AsciiBlock art = new Lines(miniArt);
-    AsciiBlock artMutated = new OurBlock(art, 1);
+    AsciiBlock artMutated = new OurBlock(art, 390);
 
-    AsciiBlock art2 = new OurBlock(artMutated, -1);
+    AsciiBlock art2 = new OurBlock(artMutated,1);
 
-    AsciiBlock.print(pen, art2);
-    AsciiBlock.print(pen, artMutated);
+    AsciiBlock art3 = new Surrounded(art2, 'b');
+    AsciiBlock art4 = new OurBlock(art3,1);
+    AsciiBlock art5 = new OurBlock(art4, 1);
+    AsciiBlock art6 = new OurBlock(art5, 1);
 
-    // Optionally flip the artwork horizontally
-    // AsciiBlock flippedArt = new HFlip(art);
+    AsciiBlock[] Hcomp1 = {art3, art4, art5, art6};
+    AsciiBlock Hcomp1Group = new HComp(VAlignment.CENTER, Hcomp1);
+    AsciiBlock Hcomp2Group = new OurBlock(Hcomp1Group, 1);
 
-    // Print the original artwork
-    //pen.println("Original Artwork:");
-    //AsciiBlock.print(pen, art);
+    AsciiBlock[] HcompBig = {Hcomp1Group, Hcomp2Group};
 
-    // Print the flipped artwork
-    // pen.println("\nFlipped Artwork:");
-    // AsciiBlock.print(pen, flippedArt);
+    AsciiBlock row1 = new HComp(VAlignment.BOTTOM, HcompBig);
 
+    AsciiBlock row2 = new OurBlock(row1, 494);
+    AsciiBlock row3 = new OurBlock(row1, 1);
+
+    AsciiBlock.print(pen, row1);
+    AsciiBlock.print(pen, row2);
+    AsciiBlock.print(pen, row3);
+
+    
+
+
+    pen.println("done");
     pen.close();
   } // main(String[])
 } // class Art80x24
