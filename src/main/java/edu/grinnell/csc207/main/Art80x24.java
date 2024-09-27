4215fd39386e970462150a5fb2362704dfa6de4d
linricha <linricha@grinnell.edu>
2024-09-26 20:16:36 -0500

    Added comments to Art80x24 as well as the art as a comment

--- src/main/java/edu/grinnell/csc207/main/Art80x24.java
+++ src/main/java/edu/grinnell/csc207/main/Art80x24.java
@@ -22,33 +22,34 @@ public class Art80x24 {
   /**
    * Create the artwork.
    *
-   * @param args
-   *   Command-line arguments (currently ignored).
+   * @param args Command-line arguments (currently ignored).
    *
-   * @exception Exception
-   *   If something goes wrong with one of the underlying classes.
+   * @exception Exception If something goes wrong with one of the underlying classes.
    */
   public static void main(String[] args) throws Exception {
     PrintWriter pen = new PrintWriter(System.out, true);
 
-    // Create a rectangular block filled with '^'
-    // AsciiBlock art = new Rect('^', 80, 24);
+    
     String outer = new String("  mmmm  ");
     String outerMid = new String(" m    m ");
-    String mid = new String("m      m");
-    
+    String mid = new String("m      m");  
+
+    // Create mini image to be used
     String[] miniArt = {outer, outerMid, mid, mid, outerMid, outer};
 
     AsciiBlock art = new Lines(miniArt);
     AsciiBlock artMutated = new OurBlock(art, 390);
 
-    AsciiBlock art2 = new OurBlock(artMutated,1);
+    // Mutate the miniArt
+    AsciiBlock art2 = new OurBlock(artMutated, 1);
 
+    // Mutate the surrounded art
     AsciiBlock art3 = new Surrounded(art2, 'b');
-    AsciiBlock art4 = new OurBlock(art3,1);
+    AsciiBlock art4 = new OurBlock(art3, 1);
     AsciiBlock art5 = new OurBlock(art4, 1);
     AsciiBlock art6 = new OurBlock(art5, 1);
 
+    // Mutate and Create full row of art
     AsciiBlock[] Hcomp1 = {art3, art4, art5, art6};
     AsciiBlock Hcomp1Group = new HComp(VAlignment.CENTER, Hcomp1);
     AsciiBlock Hcomp2Group = new OurBlock(Hcomp1Group, 1);
@@ -57,17 +58,46 @@ public class Art80x24 {
 
     AsciiBlock row1 = new HComp(VAlignment.BOTTOM, HcompBig);
 
+    // Mutate and Stack rows together
     AsciiBlock row2 = new OurBlock(row1, 494);
     AsciiBlock row3 = new OurBlock(row1, 1);
 
-    AsciiBlock.print(pen, row1);
-    AsciiBlock.print(pen, row2);
-    AsciiBlock.print(pen, row3);
+    AsciiBlock[] totalArt = {row1, row2, row3};
+
+    AsciiBlock bigArt = new VComp(HAlignment.LEFT, totalArt);
+
+    AsciiBlock.print(pen, bigArt);
 
     
+    // This is the image printed since it takes too long to print sometimes.
+    // Looks better printed since the overflow of characters on the edge is managed by the terminals spacing of characters.
+
+    // bbbbbbbbbbccccccccccddddddddddeeeeeeeeeeccccccccccddddddddddeeeeeeeeeeffffffffff
+    // bƧƧǴǴǴǴƧƧbcƨƨǵǵǵǵƨƨcdƩƩǶǶǶǶƩƩdeƪƪǷǷǷǷƪƪecƨƨǵǵǵǵƨƨcdƩƩǶǶǶǶƩƩdeƪƪǷǷǷǷƪƪefƫƫǸǸǸǸƫƫf
+    // bƧǴƧƧƧƧǴƧbcƨǵƨƨƨƨǵƨcdƩǶƩƩƩƩǶƩdeƪǷƪƪƪƪǷƪecƨǵƨƨƨƨǵƨcdƩǶƩƩƩƩǶƩdeƪǷƪƪƪƪǷƪefƫǸƫƫƫƫǸƫf
+    // bǴƧƧƧƧƧƧǴbcǵƨƨƨƨƨƨǵcdǶƩƩƩƩƩƩǶdeǷƪƪƪƪƪƪǷecǵƨƨƨƨƨƨǵcdǶƩƩƩƩƩƩǶdeǷƪƪƪƪƪƪǷefǸƫƫƫƫƫƫǸf
+    // bǴƧƧƧƧƧƧǴbcǵƨƨƨƨƨƨǵcdǶƩƩƩƩƩƩǶdeǷƪƪƪƪƪƪǷecǵƨƨƨƨƨƨǵcdǶƩƩƩƩƩƩǶdeǷƪƪƪƪƪƪǷefǸƫƫƫƫƫƫǸf
+    // bƧǴƧƧƧƧǴƧbcƨǵƨƨƨƨǵƨcdƩǶƩƩƩƩǶƩdeƪǷƪƪƪƪǷƪecƨǵƨƨƨƨǵƨcdƩǶƩƩƩƩǶƩdeƪǷƪƪƪƪǷƪefƫǸƫƫƫƫǸƫf
+    // bƧƧǴǴǴǴƧƧbcƨƨǵǵǵǵƨƨcdƩƩǶǶǶǶƩƩdeƪƪǷǷǷǷƪƪecƨƨǵǵǵǵƨƨcdƩƩǶǶǶǶƩƩdeƪƪǷǷǷǷƪƪefƫƫǸǸǸǸƫƫf
+    // bbbbbbbbbbccccccccccddddddddddeeeeeeeeeeccccccccccddddddddddeeeeeeeeeeffffffffff
+    // ɐɐɐɐɐɐɐɐɐɐɑɑɑɑɑɑɑɑɑɑɒɒɒɒɒɒɒɒɒɒɓɓɓɓɓɓɓɓɓɓɑɑɑɑɑɑɑɑɑɑɒɒɒɒɒɒɒɒɒɒɓɓɓɓɓɓɓɓɓɓɔɔɔɔɔɔɔɔɔɔ
+    // ɐΕΕϢϢϢϢΕΕɐɑΖΖϣϣϣϣΖΖɑɒΗΗϤϤϤϤΗΗɒɓΘΘϥϥϥϥΘΘɓɑΖΖϣϣϣϣΖΖɑɒΗΗϤϤϤϤΗΗɒɓΘΘϥϥϥϥΘΘɓɔΙΙϦϦϦϦΙΙɔ
+    // ɐΕϢΕΕΕΕϢΕɐɑΖϣΖΖΖΖϣΖɑɒΗϤΗΗΗΗϤΗɒɓΘϥΘΘΘΘϥΘɓɑΖϣΖΖΖΖϣΖɑɒΗϤΗΗΗΗϤΗɒɓΘϥΘΘΘΘϥΘɓɔΙϦΙΙΙΙϦΙɔ
+    // ɐϢΕΕΕΕΕΕϢɐɑϣΖΖΖΖΖΖϣɑɒϤΗΗΗΗΗΗϤɒɓϥΘΘΘΘΘΘϥɓɑϣΖΖΖΖΖΖϣɑɒϤΗΗΗΗΗΗϤɒɓϥΘΘΘΘΘΘϥɓɔϦΙΙΙΙΙΙϦɔ
+    // ɐϢΕΕΕΕΕΕϢɐɑϣΖΖΖΖΖΖϣɑɒϤΗΗΗΗΗΗϤɒɓϥΘΘΘΘΘΘϥɓɑϣΖΖΖΖΖΖϣɑɒϤΗΗΗΗΗΗϤɒɓϥΘΘΘΘΘΘϥɓɔϦΙΙΙΙΙΙϦɔ
+    // ɐΕϢΕΕΕΕϢΕɐɑΖϣΖΖΖΖϣΖɑɒΗϤΗΗΗΗϤΗɒɓΘϥΘΘΘΘϥΘɓɑΖϣΖΖΖΖϣΖɑɒΗϤΗΗΗΗϤΗɒɓΘϥΘΘΘΘϥΘɓɔΙϦΙΙΙΙϦΙɔ
+    // ɐΕΕϢϢϢϢΕΕɐɑΖΖϣϣϣϣΖΖɑɒΗΗϤϤϤϤΗΗɒɓΘΘϥϥϥϥΘΘɓɑΖΖϣϣϣϣΖΖɑɒΗΗϤϤϤϤΗΗɒɓΘΘϥϥϥϥΘΘɓɔΙΙϦϦϦϦΙΙɔ
+    // ɐɐɐɐɐɐɐɐɐɐɑɑɑɑɑɑɑɑɑɑɒɒɒɒɒɒɒɒɒɒɓɓɓɓɓɓɓɓɓɓɑɑɑɑɑɑɑɑɑɑɒɒɒɒɒɒɒɒɒɒɓɓɓɓɓɓɓɓɓɓɔɔɔɔɔɔɔɔɔɔ
+    // ccccccccccddddddddddeeeeeeeeeeffffffffffddddddddddeeeeeeeeeeffffffffffgggggggggg
+    // cƨƨǵǵǵǵƨƨcdƩƩǶǶǶǶƩƩdeƪƪǷǷǷǷƪƪefƫƫǸǸǸǸƫƫfdƩƩǶǶǶǶƩƩdeƪƪǷǷǷǷƪƪefƫƫǸǸǸǸƫƫfgƬƬǹǹǹǹƬƬg
+    // cƨǵƨƨƨƨǵƨcdƩǶƩƩƩƩǶƩdeƪǷƪƪƪƪǷƪefƫǸƫƫƫƫǸƫfdƩǶƩƩƩƩǶƩdeƪǷƪƪƪƪǷƪefƫǸƫƫƫƫǸƫfgƬǹƬƬƬƬǹƬg
+    // cǵƨƨƨƨƨƨǵcdǶƩƩƩƩƩƩǶdeǷƪƪƪƪƪƪǷefǸƫƫƫƫƫƫǸfdǶƩƩƩƩƩƩǶdeǷƪƪƪƪƪƪǷefǸƫƫƫƫƫƫǸfgǹƬƬƬƬƬƬǹg
+    // cǵƨƨƨƨƨƨǵcdǶƩƩƩƩƩƩǶdeǷƪƪƪƪƪƪǷefǸƫƫƫƫƫƫǸfdǶƩƩƩƩƩƩǶdeǷƪƪƪƪƪƪǷefǸƫƫƫƫƫƫǸfgǹƬƬƬƬƬƬǹg
+    // cƨǵƨƨƨƨǵƨcdƩǶƩƩƩƩǶƩdeƪǷƪƪƪƪǷƪefƫǸƫƫƫƫǸƫfdƩǶƩƩƩƩǶƩdeƪǷƪƪƪƪǷƪefƫǸƫƫƫƫǸƫfgƬǹƬƬƬƬǹƬg
+    // cƨƨǵǵǵǵƨƨcdƩƩǶǶǶǶƩƩdeƪƪǷǷǷǷƪƪefƫƫǸǸǸǸƫƫfdƩƩǶǶǶǶƩƩdeƪƪǷǷǷǷƪƪefƫƫǸǸǸǸƫƫfgƬƬǹǹǹǹƬƬg
+    // ccccccccccddddddddddeeeeeeeeeeffffffffffddddddddddeeeeeeeeeeffffffffffgggggggggg
 
 
-    pen.println("done");
     pen.close();
   } // main(String[])
 } // class Art80x24
